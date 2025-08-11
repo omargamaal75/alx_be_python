bff3ecae50f06c6eb9d0e560d512211b47a5d531
Omar Gamal <omargamal.r75@gmail.com>
2025-08-11 17:22:54 +0300

    Add files via upload

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,38 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -5), -10)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertAlmostEqual(self.calc.add(2.5, 3.1), 5.6)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertAlmostEqual(self.calc.subtract(5.5, 2.2), 3.3, places=2)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-3, 4), -12)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertAlmostEqual(self.calc.multiply(2.5, 4.0), 10.0)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertAlmostEqual(self.calc.divide(5.5, 2.0), 2.75)
+        
+        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertIsNone(self.calc.divide(0, 0))
+
+if __name__ == '__main__':
+    unittest.main()
